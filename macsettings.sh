a8dbae211410db640f2fb58baa6b4bd83e8b1cb6
Nitin Gaikwad <62459792+niteen1593@users.noreply.github.com>
2024-04-07 19:06:24 +0530

    Update macsettings.sh

--- macsettings.sh
+++ macsettings.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Close any open System Preferences panes, to prevent them from overriding
 # settings we’re about to change
 osascript -e 'tell application "System Preferences" to quit'
